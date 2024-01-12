31a0d3c475a444d8a7a50ead13ed98b379772380
Afonso Lima <afonsolima@gmail.com>
2024-01-12 01:07:25 -0300

    alterado arquivo fakedata.ts para verificar/contornar erro na producao

--- src/data/fakedata.ts
+++ src/data/fakedata.ts
@@ -10,7 +10,19 @@ export const fakeData: NFCe[] = [
         produto: {
           id: 'f374372e-13e0-453c-ba29-0b93d8c0198b',
           barcode: '1234567890123',
-          descricao: 'Produto Fake',
+          descricao: 'Arroz Fake',
+          imagem: 'https://cdn-cosmos.bluesoft.com.br/products/7896067200162',
+          valor: 5.49,
+        },
+        valor: 5.49,
+        quantidade: 1,
+      },
+      {
+        id: 'c857fee8-98ac-4e80-8324-6a9d841c04a4',
+        produto: {
+          id: 'f374372e-13e0-453c-ba29-0b93d8c0198b',
+          barcode: '1234567890123',
+          descricao: 'Feijao Fake',
           imagem: 'https://cdn-cosmos.bluesoft.com.br/products/7896067200162',
           valor: 5.49,
         },
